9ace13dc671200d7a942731c484be38674fddffc
Taffanie Ben <taffanie.ben@gmail.com>
2020-03-25 13:14:20 +0000

    poketimes complete

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero minus, aliquam animi sunt culpa iusto fugiat eaque unde autem illum, laboriosam eius optio, soluta rerum nobis similique architecto perferendis placeat!</p>
+    </div>   
+  )
+}
+
+export default Rainbow(About); 
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
+
+class Home extends Component {
+  state = {
+    posts: [ ]
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res.data.slice(0,10));
+        this.setState({
+          posts: res.data.slice(0,10)
+        })
+      })
+  }
+
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A pokeball"/>
+            <div className="card-content">
+              <Link to={'/' + post.id}>
+                <span className="card-title red-text">{post.title}</span>
+              </Link>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    )
+    return (
+      <div className="container home">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>   
+    )
+  }
+}
+
+export default Home; 
